96f5b42b85d250c6ef44e39ff0b19daa78aae6fc
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-30 12:34:18 +0000

    split components to smaller components

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+const RenderDish = ({ dish }) => {
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+const RenderComments = ({ comments }) => {
+
+    const commentsOutput = comments.map((cItem) => {
+        const formatedDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(cItem.date)))
+        return (
+            <div key={cItem.id}>
+                <p>{cItem.comment}</p>
+                <p>--{cItem.author}, {formatedDate}</p>
+            </div>
+        )
+    })
+    return (
+        <div>
+            <h2>Comments</h2>
+            {commentsOutput}
+        </div>
+    )
+}
+
 const Dishdetails = (props) => {
 
     if (props.dish != null) {
-
-        const comments = props.dish.comments.map((cItem) => {
-
-            const formatedDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(cItem.date)))
-            return (
-                <div key={cItem.id}>
-                    <p>{cItem.comment}</p>
-                    <p>--{cItem.author}, {formatedDate}</p>
-                </div>
-            )
-        })
-
         return (
             <div className="container">
                 <div className="row">
                     <div key="0" className="col-sm-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
-                            <CardBody>
-                                <CardTitle>{props.dish.name}</CardTitle>
-                                <CardText>{props.dish.description}</CardText>
-                            </CardBody>
-                        </Card>
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div key="1" className="col-sm-5 m-1">
-                        <h2>Comments</h2>
-                        {comments}
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
             </div>
